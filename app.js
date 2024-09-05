73e2a8c9097941519ab55f8ca456be2f00ef56ff
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-09-05 22:10:00 +0900

    wordle 선택사항 구현

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ let attempts = 0;
 let timer;
 
 function appStart() {
-  const displayGameover = () => {
-    const div = document.createElement("div");
-    div.innerText = "게임이 종료되었습니다.";
-    div.style =
-      "display:flex; justify-content:center; align-items:center; position:absolute; top:32vh; left:31vw; background-color:white; width:200px; height:100px;";
-    document.body.appendChild(div);
+  const displayGameover = (str) => {
+    const div = document.querySelector(".result");
+    console.log(str);
+    if (str === "fail") {
+      div.innerText = "게임이 종료되었습니다. 실패!";
+      div.style =
+        "display:flex; justify-content:center; align-items:center; position:absolute; top:32vh; left:31vw; background-color:white; width:200px; height:100px; animation: gameResult 2s infinite;";
+    } else if (str === "success") {
+      div.innerText = "정답을 맞췄습니다!";
+      div.style =
+        "display:flex; justify-content:center; align-items:center; position:absolute; top:32vh; left:31vw; background-color:white; width:200px; height:100px; animation: gameResult 2s;";
+    }
   };
 
   const handleBackspaceKey = () => {
@@ -27,15 +33,15 @@ function appStart() {
   };
   const nextLine = () => {
     if (attempts === 6) {
-      return gameover();
+      return gameover("fail");
     }
     attempts += 1;
     index = 0;
   };
 
-  const gameover = () => {
+  const gameover = (str) => {
     window.removeEventListener("keydown", handleKeydown);
-    displayGameover();
+    displayGameover(str);
     clearInterval(timer);
   };
 
@@ -45,24 +51,52 @@ function appStart() {
       const block = document.querySelector(
         `.board-block[data-index='${attempts}${i}']`
       );
+
       const inputChar = block.innerText;
       const answerChar = answer[i];
+      const selectKey = document.querySelector(`[data-key='${inputChar}']`);
+
       if (inputChar === answerChar) {
-        block.style.background = "#2cc364";
+        block.style.background = "#6AA964";
+        selectKey.style.background = "#6AA964";
         correct += 1;
       } else if (answer.includes(inputChar)) {
-        block.style.background = "#E6B853";
+        block.style.background = "#B49F3A";
+        selectKey.style.background = "#B49F3A";
       } else {
-        block.style.background = "#787c7e";
+        block.style.background = "#3A3A3C";
+        selectKey.style.background = "#3A3A3C";
       }
       block.style.color = "white";
     }
     if (correct === 5) {
-      gameover();
+      gameover("success");
     } else {
       nextLine();
     }
   };
+
+  const handleClick = (event) => {
+    const clickClass = event.target.className;
+    const clickKey = event.target.dataset.key;
+
+    if (clickClass === "keyboard-block") {
+      const thisBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index}']`
+      );
+      thisBlock.innerText = clickKey;
+      index += 1;
+    } else if (clickClass === "enter") {
+      if (index === 5) {
+        handleEnterKey();
+      } else {
+        return;
+      }
+    } else if (clickClass === "back") {
+      handleBackspaceKey();
+    }
+  };
+
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
@@ -110,6 +144,7 @@ function appStart() {
   startTimer();
 
   window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("click", handleClick);
 }
 
 appStart();
